f65985de90a6a9e4c752b8253694bec90084b5d7
KwakKyehyun <55428343+KwakKyehyun@users.noreply.github.com>
2019-09-19 15:32:00 +0900

    02 submission

--- main.c
+++ main.c
@@ -5,7 +5,20 @@
 
 int main(int argc, char *argv[]) {
 	
-	printf("Hello Sookmyung EE!/n");
+	
+	int x;
+	short y;
+	float z;
+	double a;
+	
+	
+	printf("byte of int : %d\n", sizeof(x) );
+	printf("byte of short : %d\n", sizeof(y) );
+	
+	
+	printf("byte of short : %d\n", sizeof(z) );
+	printf("byte of short : %d\n", sizeof(a) );
+	
 	
 	return 0;
 }
